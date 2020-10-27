6a04e2b2269a703fa8f088ba3f84021640121abf
Alejandra Camacho <camacho.ale94@gmail.com>
2020-10-26 21:13:00 -0600

    Directives in angular

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-body',
-  templateUrl: './body.component.html'
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.styl']
 })
-export class BodyComponent {}
+export class BodyComponent {
+  show = true
+
+  phrase = {
+    message: 'Un gran poder requiere una gran responsabilidad.',
+    author: '🕸 Ben Parker'
+  }
+
+  characters: string[] = ['Spiderman', 'Venom', 'Dr. Octopus']
+}
